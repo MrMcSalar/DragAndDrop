3280b5109117f2d3726dfd35fe35d2fdd0de4061
mrmcsalar <mrmcsailer@gmail.com>
2024-11-25 16:13:25 -0500

    Prof's Updates - TODO 7

--- app/src/main/java/edu/farmingdale/draganddropanim_demo/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/draganddropanim_demo/MainActivity.kt
@@ -4,14 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import edu.farmingdale.draganddropanim_demo.ui.theme.DragAndDropAnim_DemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,38 +12,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DragAndDropAnim_DemoTheme {
-                    DragAndDropBoxes()
-                }
+
+                DragAndDropBoxes()
+
+
             }
         }
     }
-
+}
 
 
 
 // This should be completed in a group setting
-// Done 1: Analyze the requirements for Individual Project 3
-// Done 2: Show the DragAndDropBoxes composable
-// Done 3: Change the circle to a rect
-// Done 4: Replace the command right with a image or icon
-// Done 5: Make this works in landscape mode only
-// ToDo 6: Rotate the rect around itself
-// ToDo 7: Move - translate the rect horizontally and vertically
+// ToDo x: Analyze the requirements for Individual Project 3
+// ToDo x: Show the DragAndDropBoxes composable
+// ToDo x: Change the circle to a rect
+// ToDo x: Replace the command right with a image or icon
+// ToDo x: Make this works in landscape mode only
+// ToDo x: Rotate the rect around itself
+// ToDo x: Move - translate the rect horizontally and vertically
 // ToDo 8: Add a button to reset the rect to the center of the screen
 // ToDo 9: Enable certain animation based on the drop event (like up or down)
 // ToDo 10: Make sure to commit for each one of the above and submit this individually
-
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!", modifier = modifier)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DragAndDropAnim_DemoTheme {
-        Greeting("Android")
-    }
-}
